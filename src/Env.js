49ffc79e9c851ea2de282c8420db1c19a08834bc
Dustin Zeisler <dustin@zeisler.net>
2024-10-05 14:59:46 -0700

    Update ports

--- src/Env.js
+++ src/Env.js
@@ -2,8 +2,8 @@ const environment = process.env.NODE_ENV;
 
 const Env = {
   development: {
-    apiHost: "http://localhost:3000",
-    fontHost: "http://localhost:3001"
+    apiHost: "http://localhost:3001",
+    fontHost: "http://localhost:3000"
   },
   production: {
     apiHost: "https://visualize_ruby.dustinzeisler.com/api",
